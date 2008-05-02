8b090c95253bf4ec2851002b1e65b27a74686a0c
com.chinaos.snaill <com.chinaos.snaill@14e3876b-2b4c-0410-8756-f7c1a983136a>
2008-05-02 03:08:46 +0000

    Added Mozilla XPCOM Wizard project

---
+++ trunk/XPComProj/XPComProj/Templates/1033/rootImpl.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "[!output PROJECT_NAME]Impl.h"
+
+////////////////////////////////////////////////
+NS_IMPL_ISUPPORTS1(C[!output PROJECT_NAME], I[!output PROJECT_NAME])
+
+C[!output PROJECT_NAME]::C[!output PROJECT_NAME](void)
+{
+}
+
+C[!output PROJECT_NAME]::~C[!output PROJECT_NAME](void)
+{
+}
+
